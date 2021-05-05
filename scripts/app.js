84ddb035f380a9180c5d9efffdb9e65d981d6d4a
Ciarm-nik <nikcia@libero.it>
2021-05-05 16:02:53 +0200

    Milestone 2

--- scripts/app.js
+++ scripts/app.js
@@ -2,5 +2,14 @@ const app = new Vue({
     el: "#app",
     data: {
         usersList: globalUsersList,
+        selectUser: ""
+    },
+    methods: {
+       onUserClick(user) {
+           this.selectUser = user
+       }
+    },
+    mounted(){
+        this.selectUser = this.usersList[0]
     }
 })
